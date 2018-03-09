ce3e8617489cfb372391e10539cb81f025b97799
Bastián Arce <bestian.design@gmail.com>
2018-03-08 22:29:06 -0300

    ´Tarea completada

---
+++ landing-page/js/script.js
@@ -0,0 +1,37 @@
+$('title').html('Transfórmate en desarrollador web');
+
+$('.bg-change').click(function() {
+  $('.main').addClass('new-bg');
+});
+
+$('.logo-change').click(function() {
+  $('.navbar-brand').find('img').attr('src', 'img/en1mes-logo.png');
+});
+
+$('.p-add').append(
+  '<section class="container seccion-info text-center">'
+   +'<h3>SOLICITA INFORMACIÓN E INSCRÍBETE</h3>'
+   +'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rem delectus veritatis dolorem ipsum, quod incidunt, aut autem rerum id quis reiciendis, voluptates error quo. Blanditiis sequi, adipisci quam sit.</p>'
+   +'<a href="#" class="btn btn-info btn-carousel btn-inscribete">Inscríbete</a>'
+   +'</section>');
+
+$('body').on('click', '.btn-inscribete', function(event) {
+  event.preventDefault();
+  $('.p-add').append(
+    '<section class="container seccion-info text-center">'
+     +'<h3>SOLICITA INFORMACIÓN E INSCRÍBETE</h3>'
+     +'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rem delectus veritatis dolorem ipsum, quod incidunt, aut autem rerum id quis reiciendis, voluptates error quo. Blanditiis sequi, adipisci quam sit.</p>'
+     +'<a href="#" class="btn btn-info btn-carousel btn-inscribete">Inscríbete</a>'
+     +'</section>');
+});
+
+$('.form-inline').on('click', '.btn-enviar', function(event) {
+  val = $('#exampleInputEmail2').val();
+  event.preventDefault();
+  alert("Tu correo "+ val +" ha sido correctamente recibido")
+});
+
+$('body').on('click', 'a', function(event) {
+  event.preventDefault();
+  alert(event.target);
+});
