e39e9a885dfd73c967dd3b76db72264cdbfff34e
Bastián Arce <bestian.design@gmail.com>
2018-03-09 01:24:34 -0300

    Add the last point in this activity

--- landing-page/js/script.js
+++ landing-page/js/script.js
@@ -1,37 +1,48 @@
-$('title').html('Transfórmate en desarrollador web');
+$(document).ready(function() {
 
-$('.bg-change').click(function() {
-  $('.main').addClass('new-bg');
-});
+  $('title').html('Transfórmate en desarrollador web');
 
-$('.logo-change').click(function() {
-  $('.navbar-brand').find('img').attr('src', 'img/en1mes-logo.png');
-});
+  $('.bg-change').click(function() {
+    $('.main').toggleClass('new-bg');
+  });
 
-$('.p-add').append(
-  '<section class="container seccion-info text-center">'
-   +'<h3>SOLICITA INFORMACIÓN E INSCRÍBETE</h3>'
-   +'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rem delectus veritatis dolorem ipsum, quod incidunt, aut autem rerum id quis reiciendis, voluptates error quo. Blanditiis sequi, adipisci quam sit.</p>'
-   +'<a href="#" class="btn btn-info btn-carousel btn-inscribete">Inscríbete</a>'
-   +'</section>');
+  $('.logo-change').click(function() {
+    $('.navbar-brand').find('img').toggle().attr('src', 'img/en1mes-logo.png');
+  });
 
-$('body').on('click', '.btn-inscribete', function(event) {
-  event.preventDefault();
   $('.p-add').append(
     '<section class="container seccion-info text-center">'
      +'<h3>SOLICITA INFORMACIÓN E INSCRÍBETE</h3>'
      +'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rem delectus veritatis dolorem ipsum, quod incidunt, aut autem rerum id quis reiciendis, voluptates error quo. Blanditiis sequi, adipisci quam sit.</p>'
      +'<a href="#" class="btn btn-info btn-carousel btn-inscribete">Inscríbete</a>'
      +'</section>');
-});
 
-$('.form-inline').on('click', '.btn-enviar', function(event) {
-  val = $('#exampleInputEmail2').val();
-  event.preventDefault();
-  alert("Tu correo "+ val +" ha sido correctamente recibido")
-});
+  $('body').on('click', '.btn-inscribete', function(event) {
+    event.preventDefault();
+    $('.p-add').append(
+      '<section class="container seccion-info text-center">'
+       +'<h3>SOLICITA INFORMACIÓN E INSCRÍBETE</h3>'
+       +'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rem delectus veritatis dolorem ipsum, quod incidunt, aut autem rerum id quis reiciendis, voluptates error quo. Blanditiis sequi, adipisci quam sit.</p>'
+       +'<a href="#" class="btn btn-info btn-carousel btn-inscribete">Inscríbete</a>'
+       +'</section>');
+  });
 
-$('body').on('click', 'a', function(event) {
-  event.preventDefault();
-  alert(event.target);
+  $('.form-inline').on('click', '.btn-enviar', function(event) {
+    val = $('#exampleInputEmail2').val();
+    event.preventDefault();
+    alert("Tu correo "+ val +" ha sido correctamente recibido")
+  });
+
+  $('body').on('click', 'a', function(event) {
+    event.preventDefault();
+    alert(event.target);
+  });
+
+  $('.list').children().children().html('Inicio');
+
+
+
+
+
+
 });
